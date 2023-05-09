037caf0a9262b8e79996dc810fc573b3adccadae
Aranjhonn <aranjhonn@yahoo.com>
2023-05-09 14:55:51 -0300

    Orientação a Objeto - Sobrecarga de Métodos

---
+++ src/com/aranjhonn/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package com.aranjhonn.maratonajava.javacore.Csobrecargametodos.test;
+
+import com.aranjhonn.maratonajava.javacore.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        anime.init("Akudama Drive", "TV", 12, "Ação");
+
+        anime.imprime();
+    }
+}
